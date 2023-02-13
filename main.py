45a5d5b04a6fde10e03a05e530c937aedaed87b5
Roberts Lācis <roberts.lacis@edu.rtu.lv>
2023-02-13 19:59:33 +0200

    Final commit #1

--- main.py
+++ main.py
@@ -13,18 +13,27 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # ESMU LABS
+            opening_brackets_stack.append(next)
             pass
 
         if next in ")]}":
-            # Process closing bracket, write your code here
+            if len(opening_brackets_stack) != 0:
+                if are_matching(opening_brackets_stack[-1], next):
+                    opening_brackets_stack = opening_brackets_stack[:-1]
+                else:
+                    return i+1
+            else:
+                return i+1
             pass
+    if len(opening_brackets_stack) != 0:
+        return text.find(opening_brackets_stack[0])+1
+    return "Success"
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
